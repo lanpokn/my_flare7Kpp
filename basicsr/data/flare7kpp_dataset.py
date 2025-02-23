c1a54545f0c633b564a8fc8ac109f38226ab7a42
lanpokn <2606629917@qq.com>
2025-02-23 23:05:58 +0800

    add the base img decrease caused by AE

--- basicsr/data/flare7kpp_dataset.py
+++ basicsr/data/flare7kpp_dataset.py
@@ -259,7 +259,7 @@ class Flare_Image_Loader(data.Dataset):
 		# merge_img=flare_img+base_img
 		# merge_img=torch.clamp(merge_img,min=0,max=1)
 		# can't predict auto exposure, thus use it
-		AC_gain=np.random.uniform(0.7,1.0)
+		AC_gain=np.random.uniform(0.5,1.0)
 		merge_img = ACES_profession(ACES_profession_reverse(flare_img)+ ACES_profession_reverse(AC_gain*base_img))
 		merge_img = torch.from_numpy(merge_img).float()
 		merge_img=torch.clamp(merge_img,min=0,max=1)
